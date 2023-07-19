113f937953155261ca8cbbb229fb4033a291b71e
WeeYoungSeok <dndrldzk@gmail.com>
2023-07-19 13:56:24 +0000

    update

--- main.py
+++ main.py
@@ -24,32 +24,12 @@
 # from itertools import permutations
 # print(list(permutations(items, 4)))
 
-# 탑
-import sys
+n, k = map(int, input().split())
 
-input = sys.stdin.readline
+student_list = list(map(int, input().split()))
 
-n = int(input().strip("\n"))
-    
-answer = [0 for _ in range(0, n)]
+answer = 0
 
-razers = list(map(int, input().strip("\n").split()))
-s = []
+for i in range(0, int(n // k) + 1, k):
+    answer += max(student_list[i:3]) - min(student_list[i:3])
 
-for i in range(0, len(razers)):
-    if len(s) == 0:
-        s.append([razers[i], i + 1])
-        continue
-    while len(s) > 0:
-        pop_data = s.pop()
-        if pop_data[0] > razers[i]:
-            answer[i] = pop_data[1]
-            s.append(pop_data)
-            s.append([razers[i], i + 1])
-            break
-        else:
-            if len(s) == 0:
-                s.append([razers[i], i + 1])
-                break
-
-print(" ".join(map(str, answer)))
---
+++ tony/math/problem_1.py
@@ -0,0 +1,221 @@
+# 수
+
+# 0 ~ 9까지 n개 뽑은 경우의 수
+def gen_permutations(arr, n):
+    result = []
+
+    if n == 0:
+        return [[]]
+
+    for i, elem in enumerate(arr):
+        for P in gen_permutations(arr[:i] + arr[i + 1:], n - 1):
+            result += [[elem] + P]
+
+    return result
+
+
+# 조합으로 나온 숫자들 [1, 2, 3] = int의 형태로 바꾸기
+def arrToInt(arr):
+    answer = ""
+    for i in arr:
+        answer += str(i)
+    return int(answer)
+
+
+# arr = gen_permutations([0, 1, 2], 2)
+
+# for i in arr:
+#     print(arrToInt(i))
+
+
+# 0 ~ 9까지 k가지의 숫자를 한 번씩만 사용할 경우
+# 1 <= k <= 5이므로 가장 큰 숫자는 98765
+# 98765까지의 소수를 구한다.
+# 에라토스테네스의 체
+def primeList(n):
+    # n을 포함시키기 위함
+    n += 1
+    # [False(0), False(1), True(2), True(3), True(4) ... True(n)]
+    sieve = [False, False] + [True] * (n)
+    end = int(n**0.5) + 1
+    for i in range(2, end):
+        if sieve[i] == True:
+            for j in range(i + i, n, i):
+                sieve[j] = False
+
+    return [i for i in range(n) if sieve[i] == True]
+
+
+# 결과값
+count = 0
+
+k, m = map(int, input().split())
+
+# 0 ~ 9까지 k개 뽑기
+k_permutations = []
+k_permutation = gen_permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], k)
+for i in k_permutation:
+    if i[0] != 0:
+        k_permutations.append(i)
+# print(k_permutations)
+
+# 두가지를 만족하는 수인지 판별하기 위한 True False 배열
+answer = [False] * (arrToInt(k_permutations[len(k_permutations) - 1]) + 1)
+
+answer_2 = [False] * (arrToInt(k_permutations[len(k_permutations) - 1]) + 1)
+
+prime_numbers = primeList(arrToInt(k_permutations[len(k_permutations) - 1]))
+
+# step 1
+for i in k_permutations:
+    attach_i = arrToInt(i)
+    for j in range(0, len(prime_numbers)):
+        if prime_numbers[j] >= attach_i:
+            break
+        bool = False
+        for k in range(j + 1, len(prime_numbers)):
+            if prime_numbers[j] + prime_numbers[k] > attach_i:
+                break
+            elif prime_numbers[j] + prime_numbers[k] == attach_i:
+                answer[attach_i] = True
+                bool = True
+                break
+        if bool == True:
+            break
+
+# step 2
+for i in k_permutations:
+    attach_i = arrToInt(i)
+    while attach_i % m == 0:
+        attach_i /= m
+    attach_i = int(attach_i)
+    for j in range(0, len(prime_numbers)):
+        if prime_numbers[j] >= attach_i:
+            break
+        if prime_numbers[j]**2 == attach_i:
+            answer_2[attach_i] = True
+            break
+        bool = False
+        for k in range(j + 1, len(prime_numbers)):
+            if prime_numbers[j] * prime_numbers[k] > attach_i:
+                break
+            elif prime_numbers[j] * prime_numbers[k] == attach_i:
+                answer_2[attach_i] = True
+                bool = True
+                break
+        if bool == True:
+            break
+
+for i in range(0, len(answer)):
+    if answer[i] == True and answer_2[i] == True:
+        count += 1
+
+print(count)
+
+
+
+
+
+###############
+# 다른 풀이
+# 0 ~ 9까지 n개 뽑은 경우의 수
+def gen_permutations(arr, n):
+    result = []
+
+    if n == 0:
+        return [[]]
+
+    for i, elem in enumerate(arr):
+        for P in gen_permutations(arr[:i] + arr[i + 1:], n - 1):
+            result += [[elem] + P]
+
+    return result
+
+
+# 조합으로 나온 숫자들 [1, 2, 3] = int의 형태로 바꾸기
+def arrToInt(arr):
+    answer = ""
+    for i in arr:
+        answer += str(i)
+    return int(answer)
+
+
+# arr = gen_permutations([0, 1, 2], 2)
+
+# for i in arr:
+#     print(arrToInt(i))
+
+
+# 0 ~ 9까지 k가지의 숫자를 한 번씩만 사용할 경우
+# 1 <= k <= 5이므로 가장 큰 숫자는 98765
+# 98765까지의 소수를 구한다.
+# 에라토스테네스의 체
+def primeList(n):
+    # n을 포함시키기 위함
+    n += 1
+    # [False(0), False(1), True(2), True(3), True(4) ... True(n)]
+    sieve = [False, False] + [True] * (n)
+    end = int(n**0.5) + 1
+    for i in range(2, end):
+        if sieve[i] == True:
+            for j in range(i + i, n, i):
+                sieve[j] = False
+
+    return [i for i in range(n) if sieve[i] == True]
+
+# 결과값
+count = 0
+
+k, m = map(int, input().split())
+
+# 0 ~ 9까지 k개 뽑기
+k_permutations = []
+k_permutation = gen_permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], k)
+for i in k_permutation:
+    if i[0] != 0:
+        k_permutations.append(i)
+
+# 두가지를 만족하는 수인지 판별하기 위한 True False 배열
+answer = [False] * (arrToInt(k_permutations[len(k_permutations) - 1]) + 1)
+# print(answer)
+
+answer_2 = [False] * (arrToInt(k_permutations[len(k_permutations) - 1]) + 1)
+# print(answer_2)
+
+temp = [i for i in range(1, arrToInt(k_permutations[len(k_permutations) - 1]) + 1)]
+# print(temp)
+for i in range(0, len(temp)):
+    a = temp[i]
+    while a % m == 0:
+        a /= m
+    temp[i] = int(a)
+# print(temp)
+
+prime_numbers = primeList(arrToInt(k_permutations[len(k_permutations) - 1]))
+
+for j in range(0, len(prime_numbers)):
+        bool = False
+        for k in range(j + 1, len(prime_numbers)):
+            if prime_numbers[j] + prime_numbers[k] > len(answer) - 1:
+                break
+            # print("에러 나기 전 : ", prime_numbers[j] + prime_numbers[k])
+            answer[prime_numbers[j] + prime_numbers[k]] = True
+
+for i in range(0, len(answer)):
+    for j in range(0, len(prime_numbers)):
+        same_number = prime_numbers[j] **2
+        if same_number in temp:
+            answer_2[same_number] = True
+        for k in range(j + 1, len(prime_numbers)):
+            diff_number = prime_numbers[j] * prime_numbers[k]
+            if diff_number in temp:
+                answer_2[diff_number] = True
+
+for i in range(0, len(answer)):
+    if answer[i] == True and answer_2[i] == True:
+        count += 1
+
+# print("결과")
+# print(answer)
+# print(answer_2)
+print(count)
