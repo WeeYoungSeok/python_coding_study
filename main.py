4310ef7e6da3740ed26e89747ac9f0551568bc6a
WeeYoungSeok <dndrldzk@gmail.com>
2023-08-06 06:34:32 +0000

    update

--- main.py
+++ main.py
@@ -78,12 +78,35 @@
 #                 print(lst[a][b], end = " ")
 #             print()
 
-# n^2 배열 자르기
+import sys
+def input():
+    return sys.stdin.readline()
 
-def solution(n, left, right):
-    answer = []
+N, K = map(int,input().split())
+num_list = list(map(int,input().split()))
 
-    for i in range(left, right + 1):
-        answer.append(max(i // n, i % n) + 1)
 
-    return answer
+
+odd_cnt = 0
+ans = 0
+start, end = 0, 0
+size = 0
+flag = 1
+
+for s in range(N):
+    while odd_cnt <= K and flag:
+        if num_list[end]%2:
+            if odd_cnt == K:
+                break
+            odd_cnt +=1
+        size += 1
+        if end == N -1:
+            flag = 0
+            break
+        end += 1
+    if ans < size - odd_cnt:
+        ans = size - odd_cnt
+    if num_list[s] % 2:
+        odd_cnt -= 1
+    size -= 1
+print(ans)
