9695c4d9284f470e31fc6d55c0248f8f785d3d94
WeeYoungSeok <dndrldzk@gmail.com>
2023-09-01 14:50:17 +0000

    update

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# update
+def solution(numbers, target):
+    lst = [0]
+    for num in numbers:
+        temp = []
+        for l in lst:
+            temp.append(l + num)
+            temp.append(l - num)
+        lst = temp
+        
+    return lst.count(target)
