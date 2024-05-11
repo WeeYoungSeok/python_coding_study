2aeb3e4a076251bef059ad3242bb077b3abf8011
WeeYoungSeok <dndrldzk@gmail.com>
2024-05-11 11:34:27 +0000

    update

--- tony/implementation/problem_11.py
+++ tony/implementation/problem_11.py
@@ -1,3 +1,11 @@
+# ZOAC
+
+# 내풀이는 조합으로 했다
+# 메모리 초과로 인해 코드를 보았다..
+# 이런건 진짜 생각하기 힘들거같다
+# 조금 더 고민해봐야겠다 ㅠ
+
+# 해답 풀이
 import sys
 
 input = sys.stdin.readline
@@ -5,6 +13,7 @@ input = sys.stdin.readline
 s = list(input().strip())
 
 result = [""] * len(s)
+
 
 def solution(arr, start):
     if not arr:
@@ -15,5 +24,6 @@ def solution(arr, start):
     print("".join(result))
     solution(arr[min_word_idx + 1:], start + min_word_idx + 1)
     solution(arr[:min_word_idx], start)
+
 
 solution(s, 0)
